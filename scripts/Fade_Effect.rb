4e48607b80153673e23b6a1498594cff7ff406d7
oumiya <oumiya.s676@gmail.com>
2016-07-02 13:05:48 +0900

    ホーム画面をリファクタ中

---
+++ scripts/Fade_Effect.rb
@@ -0,0 +1,70 @@
+require 'dxruby'
+
+# フェードイン、フェードアウトのエフェクトを表示するクラス
+class Fade_Effect
+  
+  def initialize()
+    @frame_count = 0
+    # フェードアウト用の黒い四角を準備
+    @black_box = Image.new(Game_Main::WINDOW_WIDTH, Game_Main::WINDOW_HEIGHT, C_BLACK)
+    @visible = false
+  end
+  
+  # エフェクト設定
+  # 0 が フェードアウト、1 がフェードイン
+  def setup(effect_id)
+    case effect_id
+    when 0
+      @alpha = 0
+      @effect_id = effect_id
+    when 1
+      @alpha = 255
+      @effect_id = effect_id
+    end
+    
+    @frame_count = 0
+    @visible = true
+  end
+  
+  def update()
+    if @visible then
+      draw()
+    end
+  end
+  
+  # エフェクトの描画が終了した？
+  def effect_end?()
+    
+    if @visible then
+      if @frame_count > 60 then
+        @visible = false
+        @frame_count = 0
+        return true
+      end
+    else
+      @visible = false
+      return true
+    end
+    
+    return false
+  end
+  
+  def draw()
+    Window.draw_alpha(0, 0, @black_box, @alpha, z=1000)
+    
+    if @effect_id == 0 then
+      @alpha += 4
+      if @alpha >= 240 then
+        @alpha = 255
+      end
+    else
+      @alpha -= 4
+      if @alpha < 0 then
+        @alpha = 0
+      end
+    end
+    
+    @frame_count += 1
+  end
+
+end
--- scripts/Scene_Home.rb
+++ scripts/Scene_Home.rb
@@ -1,24 +1,67 @@
 require 'dxruby'
 require_relative 'Scene_Base.rb'
 require_relative 'Scene_Battle.rb'
+require_relative 'Fade_Effect.rb'
 require_relative 'Save_Data.rb'
 include Save_Data
 
 # ホーム画面
 class Scene_Home < Scene_Base
+  # ダンジョン選択用カーソル
+  class Dungeon_Cursor
+    attr_accessor :index
+    attr_accessor :flash
+    attr_accessor :visible
+    
+    def initialize()
+      @pos = [[143, 39], [343, 39], [538, 42], [737, 44], [539, 232], [736, 233]]
+      @image = Image.load("image/system/cursor.png")
+      @visible = false
+      @flash = false
+    end
+    
+    def update()
+      if @visible then
+        draw()
+      end
+    end
+    
+    def draw()
+      if @flash then
+        if $frame_counter % 3 == 0 then
+          Window.draw(@pos[@index][0], @pos[@index][1], @image)
+        end
+      else
+        Window.draw(@pos[@index][0], @pos[@index][1], @image)
+      end
+    end
+  end
   
   # ループ前処理 例えばインスタンス変数の初期化などを行う
   def start()
-    # フェードアウト用の黒い四角
-    @black_box = Image.new(Game_Main::WINDOW_WIDTH, Game_Main::WINDOW_HEIGHT, C_BLACK)
+    # 背景画像の読み込み
     @back_image = Image.load("image/system/home.png")
-    @cursor = Image.load("image/system/cursor.png")
-    
-    # お金が足りないよウィンドウ
+    # ダンジョン選択カーソル
+    @cursor = Dungeon_Cursor.new
+    # お金が足りないよウィンドウの準備
     @not_enough_money = Image.load("image/system/not_enough_money.png")
     @not_enough_money_show = false
-    
+    # プレイヤーステータス描画用のフォントを用意
     @status_font = Font.new(18)
+    # ボタン座標の設定
+    button_setup()
+    # フェードアウト/フェードイン用の演出クラスを準備
+    @fade_effect = Fade_Effect.new
+    @fade_effect.setup(1)
+    # シーン切り替え先 0 シーン切り替えなし 1 戦闘シーン 2 装備シーン 3 ショップシーン
+    @scene_index = 0
+    # ウェイトフレームを初期化
+    @wait_frame = 0
+    
+    
+    
+    
+    
     
     @button_push = false
     @cursor_index = 0
@@ -44,44 +87,68 @@ class Scene_Home < Scene_Base
   def update()
     # ホーム画面を描画
     Window.draw(0, 0, @back_image)
+    # プレイヤーのステータスを表示
+    draw_player_status()
+    # 主人公のつぶやきを表示
+    draw_word()
+    # ダンジョン選択カーソルを表示
+    @cursor.update
     
-    # 主人公ステータスの表示
-    x = 360
-    y = 344
-
-    Window.draw_font(x, y, $player.level.to_s, @status_font) # レベル
-    y += 23
-    Window.draw_font(x, y, $player.max_hp.to_s, @status_font) # HP
-    y += 23
-    Window.draw_font(x, y, $player.attack.to_s, @status_font) # 攻撃力
-    y += 23
-    Window.draw_font(x, y, $player.defence.to_s, @status_font) # 防御力
-    y += 21
-    Window.draw_font(x, y, $player.gold.to_s, @status_font) # ゴールド
-    y += 23
-    Window.draw_font(x, y, $player.heal_count.to_s, @status_font) # 薬草の数
-    y += 21
-    Window.draw_font(x, y, $player.exp.to_s, @status_font) # 経験値
-    y += 22
-    Window.draw_font_ex(x, y, $player.get_next_level_exp().to_s, @status_font) # 次のレベルまで
+    # 指定のフレーム数ウェイト
+    if @wait_frame > 0 then
+      @wait_frame -= 1
+      return
+    end
     
-    # 主人公のつぶやき
-    Window.draw_font(208, 253, "うひひ　頑張るぞ！", @status_font, {:color => [0, 0, 0]})
-    Window.draw_font(208, 253+18, "うひひ　頑張るぞ！", @status_font, {:color => [0, 0, 0]})
-    Window.draw_font(208, 253+36, "うひひ　頑張るぞ！", @status_font, {:color => [0, 0, 0]})
+    # フェードアウト/フェードインの表示
+    @fade_effect.update
+    if @fade_effect.effect_end? then
+      if @scene_index == 1 then
+        @next_scene = Scene_Battle.new
+      end
+    else
+      return
+    end
+    
+    # キー入力処理
+    
+    # ボタン処理
+    if Input.mouse_push?(M_LBUTTON) then
+      # 洞窟を決定
+      if mouse_widthin_button?("cave") then
+        # 決定音を鳴らす
+        $sounds["decision"].play(1, 0)
+        # ダンジョンIDを1に設定
+        $dungeon_id = 1
+        # フェードアウト後に戦闘シーンへ遷移させる
+        @scene_index = 1
+        # カーソルを点滅させる
+        @cursor.flash = true
+        @cursor.visible = true
+        @cursor.index = 0
+        # BGM を停止する
+        $bgm["home"].stop(2)
+        # 画面を徐々にフェードアウトさせる
+        @fade_effect.setup(0)
+      end
+      
+      if Input.mouse_x >= 538 &&
+         Input.mouse_y >= 407 &&
+         Input.mouse_x <= 714 &&
+         Input.mouse_y <= 467 then
+         
+         $sounds["decision"].play(1, 0) # 決定音を鳴らす
+         
+         @cursor_index = 7
+         @button_push = true
+      end
+    end
+        
 
     if @control then
-      # 洞窟を選択
-      if Input.mouse_push?(M_LBUTTON) then
-        if Input.mouse_x >= 147 &&
-           Input.mouse_y >= 43 &&
-           Input.mouse_x <= 310 &&
-           Input.mouse_y <= 206 then
+
            
-           $sounds["decision"].play(1, 0) # 決定音を鳴らす
            
-           # ダンジョンIDを1に設定
-           $dungeon_id = 1
            
            @cursor_index = 1
            @button_push = true
@@ -97,16 +164,7 @@ class Scene_Home < Scene_Base
       
       # 薬草を買うボタンを選択
       if Input.mouse_push?(M_LBUTTON) then
-        if Input.mouse_x >= 538 &&
-           Input.mouse_y >= 407 &&
-           Input.mouse_x <= 714 &&
-           Input.mouse_y <= 467 then
-           
-           $sounds["decision"].play(1, 0) # 決定音を鳴らす
-           
-           @cursor_index = 7
-           @button_push = true
-        end
+
       end
     else
       if @not_control_frame <= 0 then
@@ -128,28 +186,23 @@ class Scene_Home < Scene_Base
         if @wait_count > 0 then
           # 決定音が鳴り終わるまでカーソルを点滅させる
           @wait_count -= 1
-          if @wait_count % 3 == 0 then
-            if @cursor_visible then
-              @cursor_visible = false
-            else
-              @cursor_visible = true
-            end
-          end
-          
-          if @cursor_visible then
-            Window.draw(@cursor_x, @cursor_y, @cursor)
-          end
+#          if @wait_count % 3 == 0 then
+#            if @cursor_visible then
+#              @cursor_visible = false
+#            else
+#              @cursor_visible = true
+#            end
+#          end
+#          
+#          if @cursor_visible then
+#            @cursor
+#            Window.draw(@cursor_x, @cursor_y, @cursor)
+#          end
           
         else
-          # 画面を徐々にフェードアウトさせる
-          if @fade_out_count > 0 then
-            Window.draw_alpha(0, 0, @black_box, @alpha, z=1000)
-            @fade_out_count -= 1
-            @alpha += 4
-          else
-            Window.draw_alpha(0, 0, @black_box, 255, z=1000)
-            @next_scene = Scene_Battle.new
-          end
+          @scene_index = 1
+          
+
         end
       when 7
         # ゴールドが足りているか？
@@ -168,12 +221,30 @@ class Scene_Home < Scene_Base
     else
       if @control then
         # マウスカーソルホバー
-        if Input.mouse_x >= 147 &&
-           Input.mouse_y >= 43 &&
-           Input.mouse_x <= 310 &&
-           Input.mouse_y <= 206 then
-           # 洞窟にマウスカーソルがホバーされている
-           Window.draw(143, 39, @cursor)
+        @cursor.visible = false
+        if mouse_widthin_button?("cave") then
+           @cursor.index = 0
+           @cursor.visible = true
+        end
+        if mouse_widthin_button?("forest") then
+           @cursor.index = 1
+           @cursor.visible = true
+        end
+        if mouse_widthin_button?("mansion") then
+           @cursor.index = 2
+           @cursor.visible = true
+        end
+        if mouse_widthin_button?("volcano") then
+           @cursor.index = 3
+           @cursor.visible = true
+        end
+        if mouse_widthin_button?("ice_world") then
+           @cursor.index = 4
+           @cursor.visible = true
+        end
+        if mouse_widthin_button?("castle") then
+           @cursor.index = 5
+           @cursor.visible = true
         end
       end
     end
@@ -183,13 +254,81 @@ class Scene_Home < Scene_Base
   
   # ループ後処理
   def terminate()
-    $bgm["home"].stop(0)
+    
   end
   
   # 次のシーンに遷移
   # 遷移しない場合は nil を返す
   def get_next_scene()
     return @next_scene
+  end
+  
+  # 各ボタンの座標配列を初期化します
+  # 0: 左上X座標 1: 左上Y座標 2: 右下X座標 3: 右下Y座標
+  def button_setup()
+    @button = Hash.new
+    
+    # ダンジョンボタン
+    @button["cave"] = [147, 43, 310, 206]        # 洞窟
+    @button["forest"] = [347, 43, 510, 206]      # 迷いの森
+    @button["mansion"] = [542, 46, 705, 209]     # 死霊の館
+    @button["volcano"] = [741, 47, 904, 210]     # 火山
+    @button["ice_world"] = [543, 236, 706, 399]  # 氷の世界
+    @button["castle"] = [740, 237, 903, 400]     # 魔王城
+    
+    # メニューボタン
+    @button["buy_heal"] = [538, 407, 714, 467]   # 薬草を買うボタン
+    @button["equip"] = [739, 407, 915, 467]      # 装備変更ボタン
+    @button["shop"] = [538, 471, 714, 531]       # ショップボタン
+    @button["quit"] = [739, 471, 915, 531]       # ゲームをやめるボタン
+    
+  end
+  
+  # マウスカーソルがボタンの座標内に入っているかどうかを返します
+  def mouse_widthin_button?(button_name)
+    hitbox = @button[button_name]
+    
+    res = false
+    
+    if Input.mouse_x >= hitbox[0] &&
+       Input.mouse_y >= hitbox[1] &&
+       Input.mouse_x <= hitbox[2] &&
+       Input.mouse_y <= hitbox[3] then
+       
+       res = true
+       
+    end
+    
+    return res
+  end
+  
+  # 主人公ステータスの表示
+  def draw_player_status()
+    x = 360
+    y = 344
+
+    Window.draw_font(x, y, $player.level.to_s, @status_font) # レベル
+    y += 23
+    Window.draw_font(x, y, $player.max_hp.to_s, @status_font) # HP
+    y += 23
+    Window.draw_font(x, y, $player.attack.to_s, @status_font) # 攻撃力
+    y += 23
+    Window.draw_font(x, y, $player.defence.to_s, @status_font) # 防御力
+    y += 21
+    Window.draw_font(x, y, $player.gold.to_s, @status_font) # ゴールド
+    y += 23
+    Window.draw_font(x, y, $player.heal_count.to_s, @status_font) # 薬草の数
+    y += 21
+    Window.draw_font(x, y, $player.exp.to_s, @status_font) # 経験値
+    y += 22
+    Window.draw_font_ex(x, y, $player.get_next_level_exp().to_s, @status_font) # 次のレベルまで
+  end
+  
+  # プレイヤーの一言を描画
+  def draw_word()
+    Window.draw_font(208, 253, "うひひ　頑張るぞ！", @status_font, {:color => [0, 0, 0]})
+    Window.draw_font(208, 253+18, "うひひ　頑張るぞ！", @status_font, {:color => [0, 0, 0]})
+    Window.draw_font(208, 253+36, "うひひ　頑張るぞ！", @status_font, {:color => [0, 0, 0]})
   end
 
 end
